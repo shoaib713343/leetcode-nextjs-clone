054bc7ac8371987053b9c05c9ac32035006c1980
Shoaib <shoaib713343@gmail.com>
2025-12-11 14:44:59 +0530

    authentication using clerk

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Click Me</Button>
+      <UserButton />
     </div>
   )
 }
